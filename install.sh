13050e3257473a7c3173166ba90f3a3b12bf34f5
patarisac <patar.isac@gmail.com>
2022-06-21 00:55:59 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,4 @@
 
 inst_path="/home/$(whoami)/.local/lib/python3.8/site-packages/patsac"
 
-if [ ! -e $inst_path ]
-then
-    mkdir $inst_path
-fi
-
-rm -r $inst_path/*
-cp -r src/* $inst_path
+cp -r src $inst_path
